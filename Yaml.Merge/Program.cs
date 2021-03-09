564ad464507f7fc57f5b453876e3b79e3fd5f320
Ventsislav Mladenov <ventsislav.mladenov@gmail.com>
2021-03-09 20:16:12 +0100

    Yaml merge bug

---
+++ Yaml.Merge/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using YamlDotNet.Core;
+using YamlDotNet.Serialization;
+using YamlDotNet.Serialization.NamingConventions;
+
+namespace Yaml.Merge
+{
+    struct Item
+    {
+        public int X { get; set; }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var deserializer = new DeserializerBuilder()
+                .WithNamingConvention(CamelCaseNamingConvention.Instance)
+                .Build();
+            var filePath = Path.Combine(AppContext.BaseDirectory, "input.yaml");
+            using var contents = File.OpenText(filePath);
+            var configuration = deserializer.Deserialize<Dictionary<string, Item>>(new MergingParser(new Parser(contents)));
+            Console.WriteLine(configuration["item"].X);
+        }
+    }
+}
